a9e12760eb76550e89a63ae06f788473958c4f26
Maximiliano Cóppola <maxicopp@outlook.com>
2023-03-06 20:08:31 -0300

    Adding an Event Listener

--- 01-basics/shared.js
+++ 01-basics/shared.js
@@ -1,3 +1,10 @@
 var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
 
-backdrop.style.display = 'block';
+for (let i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener('click', function () {
+    modal.style.display = 'block';
+    backdrop.style.display = 'block';
+  });
+}

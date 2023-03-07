28823e1be7cb81cc0be9d5343e922b67591f6168
Maximiliano Cóppola <maxicopp@outlook.com>
2023-03-06 21:03:22 -0300

    Adding styles with JavaScript

--- 01-basics/shared.js
+++ 01-basics/shared.js
@@ -1,10 +1,20 @@
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var selectPlanButtons = document.querySelectorAll('.plan button');
+var modalNoButton = document.querySelector('.modal__action--negative');
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener('click', function () {
     modal.style.display = 'block';
     backdrop.style.display = 'block';
   });
+}
+
+backdrop.addEventListener('click', closeModal);
+
+modalNoButton.addEventListener('click', closeModal);
+
+function closeModal() {
+  modal.style.display = 'none';
+  backdrop.style.display = 'none';
 }
